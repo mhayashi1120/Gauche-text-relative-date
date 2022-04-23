c3d7d1431d1a5e3d30ba86f8aa805dd02bcba516
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-04-24 00:08:38 +0900

    test

--- test.scm
+++ test.scm
@@ -2,16 +2,24 @@
 ;;; Test gauche_text_relative_date
 ;;;
 
+(use srfi-19)
 (use gauche.test)
 
 (test-start "gauche_text_relative_date")
 (use text.relative-date)
 (test-module 'text.relative-date)
 
+(define test-now (make-date 0 1 2 3 4 5 2020 12345))
+
 ;; The following is a dummy test code.
 ;; Replace it for your tests.
-(test* "test-gauche_text_relative_date" "gauche_text_relative_date is working"
-       (relative-date->date "1 day ago"))
+#?= (relative-date->date "1year" test-now)
+#?= (relative-date->date "1 year" test-now)
+#?= (relative-date->date "1 month" test-now)
+;; TODO
+;; #?= (relative-date->date "1 monthes" test-now)
+#?= (relative-date->date "1 months" test-now)
+
 
 ;; If you don't want `gosh' to exit with nonzero status even if
 ;; the test fails, pass #f to :exit-on-failure.
