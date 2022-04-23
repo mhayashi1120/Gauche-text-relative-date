1e15571fa1e1387a231e461377f8f8ce8b11fc4a
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-04-24 00:08:34 +0900

    working a little.

--- text/relative-date.scm
+++ text/relative-date.scm
@@ -4,12 +4,16 @@
   (use srfi-19)
   (export
    relative-date->date
-   fuzzy-parse-relative-date))
+   fuzzy-parse-relative-seconds))
 (select-module text.relative-date)
+
+;; ref: https://nginx.org/en/docs/syntax.html
+;; consider unit
+;; 1year 1y, 1m, 1s ...
 
 ;; TODO just for the first purpose
 (define (relative-date->date s :optional (now (current-date)))
-  (time-utc->date (seconds->time (+ (time->seconds (date->time-utc now)) (fuzzy-parse-relative-date s)))))
+  (time-utc->date (seconds->time (+ (time->seconds (date->time-utc now)) (fuzzy-parse-relative-seconds s)))))
 
 (define (ensure-number s)
   (cond
@@ -20,12 +24,12 @@
    [else
     (error "Assert" s)]))
 
+;; Example, nginx handle year as 365day month as 30 day
 (define (ensure-unit-seconds u)
   (match u
     ["year"
      (* 24 60 60 365)]
     ["month"
-     ;; TODO how to handle it
      (* 24 60 60 30)]
     ["day"
      (* 24 60 60)]
@@ -38,27 +42,33 @@
     ))
 
 (define (ensure-direction d)
-  (cond
-   [(string=? d "later")
-    1]
-   [(string=? d "ago")
-    -1]
-   [else
-    (error "Assert" d)]))
+  (match (string-downcase d)
+    ["later"
+     1]
+    ["ago"
+     -1]
+    [else
+     (error "Assert" d)]))
 
-(define (fuzzy-parse-relative-date text)
+;; return seconds
+(define (fuzzy-parse-relative-seconds text)
   (let loop ([s text]
              [diff 0])
     (cond
      [(string-null? s)
       diff]
      ;; TODO monthes
-     [(#/^(an?|[0-9]+)[\s\t](year|month|day|hour|min|minute|sec|second)s?[\s\t](later|ago)/ s) =>
+     [(#/^(([0-9]+[ \t]*)|(?:(?:an?)[ \t]+))/i s) =>
       (^m
-       (let ([n (ensure-number (m 1))]
-             [unit (ensure-unit-seconds (m 2))]
-             [direction (ensure-direction (m 3))])
-         (loop (m 'after) (+ diff (* n unit direction)))))]
+       (let1 n (ensure-number (string-trim-both (m 1)))
+         (if-let1 m2 (#/^(year|monthe?|day|hour|min|minute|sec|second)s?[ \t]*/i (m 'after))
+           (let1 unit (ensure-unit-seconds (m2 1))
+             (if-let1 m3 (#/^(later|ago)/ (m2 'after))
+               (let1 direction (ensure-direction (m3 1))
+                 (loop (m3 'after) (+ diff (* n unit direction))))
+               ;; (error "Failed to detect direction.")
+               (loop (m2 'after) (+ diff (* n unit 1)))))
+           (error "Failed to detect unit."))))]
      [else
       (error "Not a valid input text" s)])))
 
