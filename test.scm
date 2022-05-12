4740e7ce457c2537369274e1e9edb4dbab32039c
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-05-12 12:00:57 +0900

    printing relative date and test it

--- test.scm
+++ test.scm
@@ -11,14 +11,48 @@
 
 (define test-now (make-date 0 1 2 3 4 5 2020 12345))
 
+(define (add-second d sec)
+  (let* ([date->sec (with-module text.relative-date date->seconds)]
+         [sec->date (with-module text.relative-date seconds->date)])
+    (sec->date (+ (date->sec d) sec))))
+
 ;; The following is a dummy test code.
 ;; Replace it for your tests.
 #?= (relative-date->date "1year" test-now)
 #?= (relative-date->date "1 year" test-now)
+#?= (relative-date->date "1y" test-now)
+#?= (relative-date->date "1h" test-now)
 #?= (relative-date->date "1 month" test-now)
 ;; TODO
 ;; #?= (relative-date->date "1 monthes" test-now)
 #?= (relative-date->date "1 months" test-now)
+
+#?= (relative-date->date "1y1h1m" test-now)
+#?= (relative-date->date "1y1h1m" test-now)
+
+#?= (relative-date->date "1y 1h 1m" test-now)
+
+(define (== expected result)
+  (test* #"~|result|" expected result))
+
+(== "just now" (date->relative-date (add-second test-now 0) test-now))
+(== "1 second later" (date->relative-date (add-second test-now 1) test-now))
+(== "30 seconds later" (date->relative-date (add-second test-now 30) test-now))
+(== "30 seconds ago" (date->relative-date (add-second test-now -30) test-now))
+(== "1 minute later" (date->relative-date (add-second test-now 60) test-now))
+(== "2 minutes later" (date->relative-date (add-second test-now 120) test-now))
+(== "1 day later" (date->relative-date (add-second test-now (* 24 60 60)) test-now))
+(== "2 days later" (date->relative-date (add-second test-now (* 2 24 60 60)) test-now))
+(== "1 month later" (date->relative-date (add-second test-now (* 30 24 60 60)) test-now))
+(== "1 month later" (date->relative-date (add-second test-now (* 31 24 60 60)) test-now))
+(== "1 month later" (date->relative-date (add-second test-now (* 59 24 60 60)) test-now))
+(== "2 months later" (date->relative-date (add-second test-now (* 60 24 60 60)) test-now))
+(== "2 months later" (date->relative-date (add-second test-now (* 61 24 60 60)) test-now))
+(== "12 months later" (date->relative-date (add-second test-now (* 364 24 60 60)) test-now))
+(== "1 year later" (date->relative-date (add-second test-now (* 365 24 60 60)) test-now))
+(== "1 year later" (date->relative-date (add-second test-now (* 366 24 60 60)) test-now))
+(== "1 year later" (date->relative-date (add-second test-now (* 729 24 60 60)) test-now))
+(== "2 years later" (date->relative-date (add-second test-now (* 730 24 60 60)) test-now))
 
 
 ;; If you don't want `gosh' to exit with nonzero status even if
--- text/relative-date.scm
+++ text/relative-date.scm
@@ -1,19 +1,81 @@
+;;;
+;;; Relative (and fuzzy) datetime (as text)
+;;;
+
 (define-module text.relative-date
   (use srfi-13)
   (use util.match)
   (use srfi-19)
   (export
-   relative-date->date
-   fuzzy-parse-relative-seconds))
+   relative-date->date date->relative-date
+   fuzzy-parse-relative-seconds
+   ))
 (select-module text.relative-date)
 
 ;; ref: https://nginx.org/en/docs/syntax.html
 ;; consider unit
 ;; 1year 1y, 1m, 1s ...
 
+(define (date->seconds d)
+  ($ floor->exact $ time->seconds $ date->time-utc d))
+
+(define (seconds->date s)
+  ($ time-utc->date $ seconds->time $ floor->exact s))
+
+(define (print-relative-date d :optional (now (current-date)))
+  (let* ([diff-sec (- (date->seconds d) (date->seconds now))]
+         [diff-abs (abs diff-sec)]
+         [sign (cond
+                [(positive? diff-sec) 1]
+                [(negative? diff-sec) -1]
+                [else 0])]
+         )
+    (cond
+     [(= diff-abs 0)
+      (format #t "just now")]
+     [(< diff-abs 60)
+      (format #t "~a second" diff-abs)
+      (when (< 1 diff-abs)
+        (format #t "s"))]
+     [(< diff-abs (* 60 60))
+      (let1 diff-min (div diff-abs (* 60))
+        (format #t "~a minute" diff-min)
+        (when (< 1 diff-min)
+          (format #t "s")))]
+     [(< diff-abs (* 24 60 60))
+      (let1 diff-hour (div diff-abs (* 60 60))
+        (format #t "~a hour" diff-hour)
+        (when (< 1 diff-hour)
+          (format #t "s")))]
+     [(< diff-abs (* 24 60 60 30))
+      (let1 diff-day (div diff-abs (* 24 60 60))
+        (format #t "~a day" diff-day)
+        (when (< 1 diff-day)
+          (format #t "s")))]
+     [(< diff-abs (* 24 60 60 365))
+      (let1 diff-month (div diff-abs (* 24 60 60 30))
+        (format #t "~a month" diff-month)
+        (when (< 1 diff-month)
+          (format #t "s")))]
+     [else
+      (let1 diff-year (div diff-abs (* 24 60 60 365))
+        (format #t "~a year" diff-year)
+        (when (< 1 diff-year)
+          (format #t "s")))])
+    (cond
+     [(positive? sign)
+      (format #t " later")]
+     [(negative? sign)
+      (format #t " ago")])))
+
+(define (date->relative-date d :optional (now (current-date)))
+  (with-output-to-string
+    (^[]
+      (print-relative-date d now))))
+
 ;; TODO just for the first purpose
 (define (relative-date->date s :optional (now (current-date)))
-  (time-utc->date (seconds->time (+ (time->seconds (date->time-utc now)) (fuzzy-parse-relative-seconds s)))))
+  ($ seconds->date $ (cut + (date->seconds now) <>) $ fuzzy-parse-relative-seconds s))
 
 (define (ensure-number s)
   (cond
@@ -24,20 +86,27 @@
    [else
     (error "Assert" s)]))
 
+(define (trim-plural s)
+  (cond
+   [(#/s$/i s) =>
+    (^m (m 'before))]
+   [else
+    s]))
+
 ;; Example, nginx handle year as 365day month as 30 day
 (define (ensure-unit-seconds u)
-  (match u
-    ["year"
+  (match (string-downcase (trim-plural u))
+    [(or "year" "y")
      (* 24 60 60 365)]
-    ["month"
+    [(or "month" "m")
      (* 24 60 60 30)]
-    ["day"
+    [(or "day" "d")
      (* 24 60 60)]
-    ["hour"
+    [(or "hour" "h")
      (* 60 60)]
-    [(or "min" "minute")
+    [(or "min" "minute")                ;TODO "M"
      60]
-    [(or "sec" "second")
+    [(or "sec" "second" "s")
      1]
     ))
 
@@ -57,18 +126,20 @@
     (cond
      [(string-null? s)
       diff]
-     ;; TODO monthes
      [(#/^(([0-9]+[ \t]*)|(?:(?:an?)[ \t]+))/i s) =>
       (^m
        (let1 n (ensure-number (string-trim-both (m 1)))
-         (if-let1 m2 (#/^(year|monthe?|day|hour|min|minute|sec|second)s?[ \t]*/i (m 'after))
-           (let1 unit (ensure-unit-seconds (m2 1))
-             (if-let1 m3 (#/^(later|ago)/ (m2 'after))
-               (let1 direction (ensure-direction (m3 1))
-                 (loop (m3 'after) (+ diff (* n unit direction))))
-               ;; (error "Failed to detect direction.")
-               (loop (m2 'after) (+ diff (* n unit 1)))))
-           (error "Failed to detect unit."))))]
+         (cond
+          [(#/^((year|month|day|hour|min|minute|sec|second)s?|[ymdhs])[ \t]*/i (m 'after)) =>
+           (^ [m2]
+             (let1 unit (ensure-unit-seconds (m2 1))
+               (if-let1 m3 (#/^(later|ago)/ (m2 'after))
+                 (let1 direction (ensure-direction (m3 1))
+                   (loop (m3 'after) (+ diff (* n unit direction))))
+                 ;; (error "Failed to detect direction.")
+                 (loop (m2 'after) (+ diff (* n unit 1))))))]
+          [else
+           (error "Failed to detect unit.")])))]
      [else
       (error "Not a valid input text" s)])))
 
